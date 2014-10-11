f7470bb30cd95bc069c52fb4639eec05b6000cb2
Richard Oliver <richard@rholiver.co.uk>
2014-10-11 13:39:29 +0100

    Updated to web api V2 and added web api sample project

---
+++ Samples/Glimpse.WebApiV2.Sample/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Glimpse.WebApiV2.Sample.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Samples/Glimpse.WebApiV2.Sample/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Glimpse.WebApiV2.Sample.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumTypeModelDescription : ModelDescription
+    {
+        public EnumTypeModelDescription()
+        {
+            Values = new Collection<EnumValueDescription>();
+        }
+
+        public Collection<EnumValueDescription> Values { get; private set; }
+    }
+}
