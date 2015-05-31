d05284729d9292468396b365b90523ffe5bee339
Richard Oliver <richard@rholiver.co.uk>
2015-05-31 19:57:09 +0100

    Switch to correct web api version for v2.1 sample project

--- source/Glimpse.WebApiV2.1.Sample/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Glimpse.WebApiV2.Sample.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
